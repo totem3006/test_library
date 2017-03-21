28aa80409ddc1ea86ac3306fedeec8099cc8ee19
totem3006 <totem3006@gmail.com>
2017-03-21 12:07:05 +0300

    Updated DDL script (changed M2M creation logic)

--- models/versions/002_Insert_Data.py
+++ models/versions/002_Insert_Data.py
@@ -24,34 +24,41 @@ M2M = Table(
     Column('book_id', Integer, ForeignKey('Book.id'), primary_key=True),
 )
 
-'''
-    "Азбука" Иванов В.П.
-    "Азбука (расширенное издание)" Иванов В.П., Сидоров С.С.
-    "Война и Мир" Толстой Л.Н.
-'''
+AUTHORS_FIXTURES = [
+    {'name': 'Иванов В.П.', 'description': 'Описание Иванова'},
+    {'name': 'Сидоров С.С.', 'description': 'Описание Петрова'},
+    {'name': 'Толстой Л.Н.', 'description': 'Описание Толстого'},
+]
+BOOKS_FIXTURES = [
+    {'name': 'Азбука', 'description': 'Главная книга'},
+    {'name': 'Азбука (расширенное издание)', 'description': 'Расширенная главная книга'},
+    {'name': 'Война и Мир', 'description': 'Роман-Эпопея'},
+]
+M2M_FIXTURE = [
+    {'book': 'Азбука', 'name': 'Иванов В.П.'},
+    {'book': 'Азбука (расширенное издание)', 'name': 'Иванов В.П.'},
+    {'book': 'Азбука (расширенное издание)', 'name': 'Сидоров С.С.'},
+    {'book': 'Война и Мир', 'name': 'Толстой Л.Н.'},
+]
+
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
-    Author.insert().values([
-        {'id': 1, 'name': 'Иванов В.П.', 'description': 'Описание Иванова'},
-        {'id': 2, 'name': 'Сидоров С.С.', 'description': 'Описание Петрова'},
-        {'id': 3, 'name': 'Толстой Л.Н.', 'description': 'Описание Толстого'},
-    ]).execute()
 
-    Book.insert().values([
-        {'id': 1, 'name': 'Азбука', 'description': 'Главная книга'},
-        {'id': 2, 'name': 'Азбука (расширенное издание)', 'description': 'Расширенная главная книга'},
-        {'id': 3, 'name': 'Война и Мир', 'description': 'Роман-Эпопея'},
-    ]).execute()
+    Author.insert().values(AUTHORS_FIXTURES).execute()
+    Book.insert().values(BOOKS_FIXTURES).execute()
 
-    M2M.insert().values([
-        {'author_id': 3, 'book_id': 3},
-        {'author_id': 1, 'book_id': 1},
-        {'author_id': 1, 'book_id': 2},
-        {'author_id': 2, 'book_id': 2},
-    ]).execute()
+    for it in  M2M_FIXTURE:
+        name = it['name']
+        book = it['book']
+
+        author_id = Author.select(Author.c.name==name).execute().first()[0]
+        book_id = Book.select(Book.c.name==book).execute().first()[0]
+
+        M2M.insert().values({'author_id': author_id, 'book_id': book_id, }).execute()
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
+
     M2M.delete().execute()
     Book.delete().execute()
     Author.delete().execute()
